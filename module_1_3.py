92e1304f37fe05628417b30ffd5272b2c8dd5767
Dmitry-74-ykt <dima.nasyalnika@gmail.com>
2024-11-21 00:18:59 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Dmitry'
+print (('Name')+':'  + (name))
+age = 50
+print (('Age')+':' + str(age))
+New_Age = age + 1
+print('New_Age'+':' + str(New_Age))
+is_student = 'True'
+print('is_student'+':' + (is_student))
